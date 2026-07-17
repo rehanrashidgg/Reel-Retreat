51ee712efbdf6cf01c3fe3ba1f2a508c50c3a496
Rehan Rashid <rehanrashd@yahoo.com>
2026-07-17 17:08:15 +0500

    Added Fishing Rod To Player
    Added fishing rod prefab to player's main camera.

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Reel-Retreat
+- Unity version: Unity 6000.3.19f1
+- Active game object:
+  - Name: Floor
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/FishingCanvas-1st/FishingCanvas-1-Controller.cs
+++ Assets/Scripts/FishingCanvas-1st/FishingCanvas-1-Controller.cs
@@ -25,6 +25,8 @@ public class FishingCanvas1Controller : MonoBehaviour
         {
             if(QualityBarTracker.QualityType == "Green")
             {
+                Canvas1SpaceBarListener = false;
+
                 ShowPerfect();
                 print("Perfect-spaceBarPressed");
             }
@@ -45,18 +47,18 @@ public class FishingCanvas1Controller : MonoBehaviour
         Sequence seq = DOTween.Sequence();
 
         // Move it up to 150f
-        seq.Append(PerfectImageRect.DOAnchorPosY(150f, 0.3f).SetEase(EaseType));
+        seq.Append(PerfectImageRect.DOAnchorPosY(215f, 0.3f).SetEase(EaseType));
 
         // Wait on screen for 0.3 second
         seq.AppendInterval(0.3f);
 
         // Move it to 316f
-        seq.Append(PerfectImageRect.DOAnchorPosY(316f, 0.3f).SetEase(EaseType));
+        seq.Append(PerfectImageRect.DOAnchorPosY(415f, 0.3f).SetEase(EaseType));
 
         // Turn it off when the entire sequence is complete
         seq.OnComplete(() => PerfectImage.SetActive(false));
 
-        Canvas1SpaceBarListener = false;
+        
 
     }
 
